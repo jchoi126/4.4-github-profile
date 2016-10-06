7fc2bd4c9a735e93cc0f710c2e4d10cade45d142
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-10-06 15:43:38 -0400

    got all the javascript stuff in, not time for styling

---
+++ app/scripts/gitapikeys.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  "token": 'c65c42b620262121038dc135a918a0d88ab998a7'
+};
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,46 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+var githubtoken = require('./gitapikeys.js');
+
+if(githubtoken !== undefined){
+ $.ajaxSetup({
+   headers: {
+     'Authorization': 'token ' + githubtoken.token
+   }
+ });
+}
+
+$.ajax('https://api.github.com/users/jchoi126').then(displayProfile);
+function displayProfile(data){
+ var profiledata = data;
+console.log(data);
+ var $profileContainer = $('#profile-bar');
+ var source = $('#profile-bar-template').html();
+ var template = Handlebars.compile(source);
+// be sure to know when to use append and prepend.
+$profileContainer.prepend(template(profiledata));
+ // _.each(profiledata, function(profilebar){
+ //   $profileContainer.append(template(profilebar));
+ // });
+}
+
+$.ajax('https://api.github.com/users/jchoi126').then(displayInfoProfile);
+function displayInfoProfile(data){
+ var $profileInfoContainer = $('#profile-info');
+ var source = $('#profile-info-template').html();
+ var template = Handlebars.compile(source);
+$profileInfoContainer.prepend(template(data));
+}
+
+$.ajax('https://api.github.com/users/jchoi126/repos?sort=pushed').then(displayRepository);
+function displayRepository(data){
+ var repositoryData = data;
+ var $repositoryContainer = $('#repositories');
+ var source = $('#repositories-template').html();
+ var template = Handlebars.compile(source);
+// be sure to know when to use append and prepend.
+ _.each(repositoryData, function(repositories){
+   $repositoryContainer.append(template(repositories));
+ });
+}
